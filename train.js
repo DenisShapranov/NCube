bb09b2cc8e69fb6c4261cfdc1d551e3f02467223
DenisShapranov <surlych@gmail.com>
2018-05-16 19:55:37 +0300

    Add files via upload

--- train.js
+++ train.js
@@ -80,7 +80,9 @@ Station11 = {
 
 var route1 = [Station1, Station2, Station3, Station4, Station5, Station6];
 
-var route2   = [Station6, Station2, Station5, Station10,  Station7, Station9, Station10];
+var route2 = [Station6, Station2, Station5, Station10, Station7, Station9, Station11];
+
+var route3 = [Station9, Station4, Station3, Station6, Station11,  Station2, Station10];
 
 function createView(obj) {
  var $result = $('<div></div>').addClass('station');
@@ -103,14 +105,14 @@ function searchStation(array, search) {
 function trainMoovingRight (route, trainNum, quanPass, scoreBoard) {
     let time = 2000;
     let i = -1;
-  
+    let wait = false;
     return function() {
             if ( ++i < route.length ) {
-             
               if  (route[i].trainNumArrive != trainNum && route[i].trainNumArrive != 0){ 
-                  i--;
-                  // route[i+1].quanPass = quanPass-1;
-              };
+                  wait = true;
+                    }
+            
+            if (!wait){
                 route[i].trainOn = true,
                 route[i].trainArrive = false;
                 route[i].trainNumArrive = 0;
@@ -126,10 +128,13 @@ function trainMoovingRight (route, trainNum, quanPass, scoreBoard) {
                     
                 } else {
                     i--;
-                }}
+                }}}else{
+                     scoreBoard.append(createView(route[i]));
+                }
+
             setTimeout( arguments.callee, time );
                
-            }else{
+            }else {
             trainMoovingLeft(route, trainNum, quanPass, scoreBoard);
             }
         }();
@@ -140,12 +145,14 @@ function trainMoovingLeft (route, trainNum, quanPass, scoreBoard ) {
     
     let time = 2000;
     let i = route.length-1;
+    let wait = false;
   
     return function() {
             if ( --i >-1 ) {
                if  (route[i].trainNumArrive != trainNum && route[i].trainNumArrive != 0){ 
-                  i++;
+                  wait = true;
               };
+             if (!wait){
                 route[i].trainArrive = false;
                 route[i].trainNumArrive = 0;
                 route[i].quanPass = 0; 
@@ -161,11 +168,9 @@ function trainMoovingLeft (route, trainNum, quanPass, scoreBoard ) {
                     
                 } else {
                     i++;
-
+                }}}else{
+                     scoreBoard.append(createView(route[i]));
                 }
-            }
-
-                
                 setTimeout( arguments.callee, 2000 );
             }
         }();
@@ -181,8 +186,10 @@ let sss = ss.getTime + 20000;
 
 var  $scoreBoard1 = $('#train1');
 var  $scoreBoard2 = $('#train2');
+var  $scoreBoard3 = $('#train3');
 
 
 
-trainMoovingRight(route1, 1, 45, $scoreBoard1);
+trainMoovingRight(route1, 1, 95, $scoreBoard1);
 trainMoovingRight(route2, 2, 36, $scoreBoard2);
+trainMoovingRight(route3, 3, 87, $scoreBoard3);
